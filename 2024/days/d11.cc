b0e1864241ba989b002f107908e32fe8dd252947
Eric Lee <nokeeo@me.com>
2024-12-11 00:06:46 -0800

    [2024] Day 11 Part 2.

--- 2024/days/d11.cc
+++ 2024/days/d11.cc
@@ -2,6 +2,7 @@
 
 #include <iostream>
 
+#include "frequency_counter.h"
 #include "parse.h"
 
 namespace {
@@ -30,21 +31,28 @@ std::pair<int64_t, int64_t> SplitNumber(int64_t i) {
   return split;
 }
 
-std::vector<int64_t> Blink(const std::vector<int64_t>& stones) {
-  std::vector<int64_t> new_stones;
-  new_stones.reserve(stones.size());
+FrequencyCounter<int64_t> Blink(const FrequencyCounter<int64_t>& stones) {
+  FrequencyCounter<int64_t> new_stones;
   for (const auto& s : stones) {
-    if (s == 0) {
-      new_stones.push_back(1);
-    } else if (static_cast<int>((log10(s)) + 1) % 2 == 0) {
-      auto[left, right] = SplitNumber(s);
-      new_stones.push_back(left);
-      new_stones.push_back(right);
+    if (s.first == 0) {
+      new_stones.Increment(1, s.second);
+    } else if (static_cast<int>((log10(s.first)) + 1) % 2 == 0) {
+      auto[left, right] = SplitNumber(s.first);
+      new_stones.Increment(left, s.second);
+      new_stones.Increment(right, s.second);
     } else {
-      new_stones.push_back(s * 2024);
+      new_stones.Increment(s.first * 2024, s.second);
     }
   }
   return new_stones;
+}
+
+int64_t GetTotalStones(const FrequencyCounter<int64_t>& stones) {
+  int64_t total = 0;
+  for (const auto& s : stones) {
+    total += s.second;
+  }
+  return total;
 }
 }  // namespace
 
@@ -53,18 +61,30 @@ namespace aoc {
 int64_t D11P1(std::ifstream& input) {
   std::string line;
   std::getline(input, line);
-  std::vector<int64_t> stones = ParseInts<int64_t>(line);
-  for (int i = 0; i < 75; ++i) {
-    stones = Blink(stones);
-    // for (const auto& v : stones) {
-    //   std::cout << v << " ";
-    // }
-    // std::cout << std::endl;
+  std::vector<int64_t> stone_line = ParseInts<int64_t>(line);
+  FrequencyCounter<int64_t> stones;
+  for (const auto& s : stone_line) {
+    stones.Increment(s);
   }
-  return stones.size();
+
+  for (int i = 0; i < 25; ++i) {
+    stones = Blink(stones);
+  }
+  return GetTotalStones(stones);
 }
 
 int64_t D11P2(std::ifstream& input) {
-  return 0;
+  std::string line;
+  std::getline(input, line);
+  std::vector<int64_t> stone_line = ParseInts<int64_t>(line);
+  FrequencyCounter<int64_t> stones;
+  for (const auto& s : stone_line) {
+    stones.Increment(s);
+  }
+
+  for (int i = 0; i < 75; ++i) {
+    stones = Blink(stones);
+  }
+  return GetTotalStones(stones);
 }
 }  // namespace aoc
--- cpp/frequency_counter.h
+++ cpp/frequency_counter.h
@@ -5,20 +5,20 @@
 namespace aoc {
   template<typename T>
   class FrequencyCounter {
-    static constexpr int32_t kNotFound = 0;
+    static constexpr int64_t kNotFound = 0;
     public:
-    void Increment(const T& v) {
-      int32_t count = 0;
+    void Increment(const T& v, int64_t amount = 1) {
+      int64_t count = 0;
       auto itr = counts_.find(v);
       if (itr != counts_.end()) {
         count = itr->second;
       }
-      counts_.insert_or_assign(v, ++count);
+      counts_.insert_or_assign(v, count + amount);
     }
 
-    void Set(const T& k, int32_t v) { counts_.insert_or_assign(k, v); }
+    void Set(const T& k, int64_t v) { counts_.insert_or_assign(k, v); }
 
-    int32_t GetCount(const T& k) const {
+    int64_t GetCount(const T& k) const {
       auto itr = counts_.find(k);
       if (itr == counts_.end()) {
         return kNotFound;
@@ -26,12 +26,12 @@ namespace aoc {
       return itr->second;
     }
 
-    typename std::unordered_map<T, int32_t>::const_iterator max() const {
+    typename std::unordered_map<T, int64_t>::const_iterator max() const {
       if (unique_size() == 0) {
         return end();
       }
 
-      typename std::unordered_map<T, int32_t>::const_iterator max = begin(); 
+      typename std::unordered_map<T, int64_t>::const_iterator max = begin(); 
       for (auto itr = begin(); itr != end(); ++itr) {
         if (max->second < itr->second) {
           max = itr;
@@ -40,12 +40,12 @@ namespace aoc {
       return max;
     }
 
-    typename std::unordered_map<T, int32_t>::const_iterator begin() const { return counts_.begin(); }
-    typename std::unordered_map<T, int32_t>::const_iterator end() const { return counts_.end(); }
+    typename std::unordered_map<T, int64_t>::const_iterator begin() const { return counts_.begin(); }
+    typename std::unordered_map<T, int64_t>::const_iterator end() const { return counts_.end(); }
     size_t unique_size() const { return counts_.size(); }
 
     private:
-    std::unordered_map<T, int32_t> counts_;
+    std::unordered_map<T, int64_t> counts_;
   };
 }
 #endif  // FREQUENCY_COUNTER_H_
