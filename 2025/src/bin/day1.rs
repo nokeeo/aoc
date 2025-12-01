44d21d578bbf0e8da092bf86cf691b244eaeb10d
Eric Lee <nokeeo@me.com>
2025-12-01 01:04:49 -0800

    [2025] Fix naming.

--- 2025/src/bin/day1.rs
+++ 2025/src/bin/day1.rs
@@ -15,7 +15,7 @@ fn part1(lines: std::io::Lines<io::BufReader<File>>) -> isize {
   let mut count = 0;
   for line in lines.map_while(Result::ok) {
    dial += parse_rotation(&line);
-   dial = normalizeDial(dial);
+   dial = normalize_dial(dial);
 
    if dial == 0 {
     count += 1;
@@ -28,25 +28,25 @@ fn part2(lines: std::io::Lines<io::BufReader<File>>) -> isize {
   let mut dial = 50;
   let mut count = 0;
   for line in lines.map_while(Result::ok) {
-    let oldDial = dial;
+    let last_dial = dial;
     dial += parse_rotation(&line);
     count += dial.abs() / 100;
     if dial == 0 {
       count += 1;
-    } else if oldDial != 0 && oldDial.signum() != dial.signum() {
+    } else if last_dial != 0 && last_dial.signum() != dial.signum() {
       count += 1;
     }
 
-    dial = normalizeDial(dial);
+    dial = normalize_dial(dial);
   }
   return count;
 }
 
-fn normalizeDial(dial: isize) -> isize {
+fn normalize_dial(dial: isize) -> isize {
   if dial >= 0 {
     return dial % 100;
   }
-  return normalizeDial(100 - (dial.abs() % 100));
+  return normalize_dial(100 - (dial.abs() % 100));
 }
 
 fn parse_rotation(line: &str) -> isize {
