3faa52c9d62af30a5b303a4c210bdd3ed7e3524c
Eric Lee <nokeeo@me.com>
2025-12-03 00:26:34 -0800

    [2025] Day 3 Part 2

--- 2025/src/bin/day03.rs
+++ 2025/src/bin/day03.rs
@@ -9,7 +9,7 @@ struct FindMaxResult {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   let args: Vec<String> = env::args().collect();
-  println!("{}", part_1(&args[1])?);
+  println!("{}", part_2(&args[1])?);
   Ok(())
 }
 
@@ -18,15 +18,33 @@ fn part_1(filename: &str) -> Result<u64, Box<dyn std::error::Error>>{
   let lines = io::BufReader::new(file).lines();
   let mut sum = 0;
   for line in lines.map_while(Result::ok) {
-    sum += get_joltage(&line);
+    sum += get_joltage(&line, /*n=*/2);
   }
   Ok(sum)
 }
 
-fn get_joltage(bank: &str) -> u64 {
-  let first = find_max(&bank[..bank.len() - 1]);
-  let second = find_max(&bank[first.i + 1..]);
-  (first.value * 10 + second.value) as u64
+fn part_2(filename: &str) -> Result<u64, Box<dyn std::error::Error>>{
+  let file = File::open(filename)?;
+  let lines = io::BufReader::new(file).lines();
+  let mut sum = 0;
+  for line in lines.map_while(Result::ok) {
+    sum += get_joltage(&line, /*n=*/12);
+  }
+  Ok(sum)
+}
+
+fn get_joltage(bank: &str, n: usize) -> u64 {
+  let mut sum: u64 = 0;
+  let mut search_str: &str = bank;
+  for i in 0..n {
+    let result = find_max(&search_str[..=(search_str.len() - (n - i))]);
+    let max = result.value as u64 * 10u64.pow((n - i - 1) as u32);
+    if result.i != search_str.len() - 1 {
+      search_str = &search_str[result.i + 1..];
+    }
+    sum += max;
+  }
+  sum
 }
 
 fn find_max(bank: &str) -> FindMaxResult {
