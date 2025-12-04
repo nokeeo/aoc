497d1cce34a6010aa14363a09c952beb3318bea7
Eric Lee <nokeeo@me.com>
2025-12-03 22:49:58 -0800

    [2025] Day 4.

---
+++ 2025/src/bin/day04.rs
@@ -0,0 +1,121 @@
+use array2d::Array2D;
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+
+// --- Day 4: Printing Department ---
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let args: Vec<String> = env::args().collect();
+  println!("{}", part_1(&args[1])?);
+  println!("{}", part_2(&args[1])?);
+  Ok(())
+}
+
+fn part_1(filename: &str) -> Result<u64, Box<dyn std::error::Error>>{
+  let mut file = File::open(filename)?;
+  let mut reader = io::BufReader::new(file);
+  let grid_size = get_grid_size(reader);
+
+  let mut grid = Array2D::filled_with('.', grid_size.0, grid_size.1);
+  file = File::open(filename)?;
+  reader = io::BufReader::new(file);
+  let mut row = 0;
+  for line in reader.lines().map_while(Result::ok) {
+    for c in 0..line.len() {
+      grid.set(row, c, line.chars().nth(c).expect("Out out bounds"));
+    }
+    row += 1;
+  }
+
+  let mut count = 0;
+  for (row_index, row) in grid.rows_iter().enumerate() {
+    for (column_index, element) in row.enumerate() {
+      if *element == '@' && adjacent_roll_count(row_index, column_index, &grid) < 4 {
+        count += 1;
+      }
+    }
+  }
+  Ok(count)
+}
+
+fn part_2(filename: &str) -> Result<u64, Box<dyn std::error::Error>> {
+  let mut file = File::open(filename)?;
+  let mut reader = io::BufReader::new(file);
+  let grid_size = get_grid_size(reader);
+
+  let mut grid = Array2D::filled_with('.', grid_size.0, grid_size.1);
+  file = File::open(filename)?;
+  reader = io::BufReader::new(file);
+  let mut row = 0;
+  for line in reader.lines().map_while(Result::ok) {
+    for c in 0..line.len() {
+      grid.set(row, c, line.chars().nth(c).expect("Out out bounds"));
+    }
+    row += 1;
+  }
+
+  let mut removed = true;
+  let mut removed_count = 0;
+  while removed {
+    removed = false;
+    let mut index_to_remove: (usize, usize) = (0, 0);
+    'gridLoop: for (row_index, row) in grid.rows_iter().enumerate() {
+      for (column_index, element) in row.enumerate() {
+        if *element == '@' && adjacent_roll_count(row_index, column_index, &grid) < 4 {
+          removed = true;
+          index_to_remove = (row_index, column_index);
+          break 'gridLoop;
+        }
+      }
+    }
+
+    if removed {
+      grid.set(index_to_remove.0, index_to_remove.1, '.');
+      removed_count += 1;
+    }
+  }
+  Ok(removed_count)
+}
+
+fn adjacent_roll_count(row: usize, column: usize, grid: &Array2D<char>) -> u32 {
+  let mut count = 0;
+  // Top.
+  if row > 0 && column > 0 {
+    count += is_roll(row - 1, column - 1, grid);
+  }
+
+  if row > 0 {
+    count += is_roll(row - 1, column, grid);
+    count += is_roll(row - 1, column + 1, grid);
+  }
+
+  if column > 0 {
+    count += is_roll(row, column - 1, grid);
+    count += is_roll(row + 1, column - 1, grid);
+  }
+  count += is_roll(row, column + 1, grid);
+
+  // Bottom.
+  count += is_roll(row + 1, column, grid);
+  count += is_roll(row + 1, column + 1, grid);
+
+  count
+}
+
+fn is_roll(row: usize, column: usize, grid: &Array2D<char>) -> u32 {
+  let Some(space) = grid.get(row, column) else {
+    return 0;
+  };
+  if *space == '@' { 1 } else { 0 }
+}
+
+fn get_grid_size(reader: io::BufReader<File>) -> (usize, usize) {
+  let mut width = 0;
+  let mut height = 0;
+  for line in reader.lines().map_while(Result::ok) {
+    width = line.len();
+    height += 1; 
+  }
+  return (width, height);
+}
