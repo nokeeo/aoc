43a687e1c1b3f25d850d522daf21ff2d2d5d696e
Eric Lee <nokeeo@me.com>
2025-12-06 00:09:45 -0800

    [2025] Day 6 Part 2

--- 2025/src/bin/day06.rs
+++ 2025/src/bin/day06.rs
@@ -45,7 +45,48 @@ fn part_1(filename: &str) -> Result<u64, Box<dyn std::error::Error>> {
 fn part_2(filename: &str) -> Result<u64, Box<dyn std::error::Error>> {
   let mut file = File::open(filename)?;
   let mut reader = io::BufReader::new(file);
-  Ok(1)
+  let mut lines: Vec<String> = Vec::new();
+  for line in reader.lines().map_while(Result::ok) {
+    lines.push(line.chars().rev().collect());
+  }
+
+  // Hardcoded for input.
+  let digits = &lines[..4];
+  let ops = &lines[4];
+  let i = 0;
+
+  let mut nums: Vec<u64> = Vec::new();
+  let mut skip = false;
+  let mut sum = 0;
+  for i in 0..lines[0].len() {
+    if skip {
+      skip = false;
+      continue;
+    }
+
+    let mut num = 0;
+    let mut pow = 0;
+    for (j, digit_line) in digits.iter().rev().enumerate() {
+      let digit = digit_line.chars().nth(i).expect("No char");
+      if digit == ' ' {
+        continue;
+      }
+
+      let digit = digit.to_digit(10).unwrap_or(0) as u64;
+      num += 10_u64.pow(pow) * digit;
+      pow += 1;
+    }
+    nums.push(num);
+
+    let op = ops.chars().nth(i).unwrap();
+    if op != ' ' {
+      sum += nums.iter().copied().reduce(|acc, n| execute(acc, n, op)).unwrap();
+      nums.clear();
+      skip = true;
+    }
+  }
+
+  Ok(sum)
 }
 
 fn execute(n1: u64, n2: u64, op: char) -> u64 {
