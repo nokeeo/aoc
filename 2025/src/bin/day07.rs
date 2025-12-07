63ce162af22c4ecea6f725c67c2189e94c753516
Eric Lee <nokeeo@me.com>
2025-12-07 13:07:17 -0800

    [2025] Day 7 Part 2

--- 2025/src/bin/day07.rs
+++ 2025/src/bin/day07.rs
@@ -3,9 +3,12 @@ use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
 
+// --- Day 7: Laboratories ---
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   let args: Vec<String> = env::args().collect();
   println!("{}", part_1(&args[1])?);
+  println!("{}", part_2(&args[1])?);
   Ok(())
 }
 
@@ -32,4 +35,27 @@ fn part_1(filename: &str) -> Result<u64, Box<dyn std::error::Error>> {
     }
   }
   Ok(count)
+}
+
+fn part_2(filename: &str) -> Result<u64, Box<dyn std::error::Error>> {
+  let mut file = File::open(filename)?;
+  let mut reader = io::BufReader::new(file);
+  let mut iter = reader.lines().map_while(Result::ok);
+
+  let first_line = iter.nth(0).expect("Need a first line");
+  let mut beams = vec![0; first_line.len()];
+  let start_i = first_line.chars().position(|c| c == 'S').expect("No start position in input");
+  beams[start_i] = 1;
+
+  for line in iter {
+    for (splitter_index, _) in line.chars().enumerate().filter(|(_, c)| *c != '.') {
+      if beams[splitter_index] == 0 {
+        continue;
+      }
+      beams[splitter_index + 1] += beams[splitter_index];
+      beams[splitter_index - 1] += beams[splitter_index];
+      beams[splitter_index] = 0;
+    }
+  }
+  Ok(beams.iter().sum())
 }
